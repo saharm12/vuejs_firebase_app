59a3f4691838a67919c059629aebd670ac81377b
saharm12 <moussa.sahar0@gmail.com>
2024-06-23 16:43:15 +0100

    disable route link

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+  <div class="container">
     <br>
     <br>
     <div class="alert alert-primary" role="alert">
@@ -10,36 +10,84 @@
 
     <div class="card">
       <div class="card-body">
+        <div class="alert alert-success" v-if="messagerSuccess" role="alert">
+        {{ messagerSuccess }}
+      </div>
         <h5 class="card-title">Sign Up</h5>
-        <form>
+        <form @submit.prevent="signUp">
           <div data-mdb-input-init class="form-outline mb-4">
-            <input type="email" id="form1Example1" class="form-control" />
+            <input v-model="user.name" type="text" id="form1Example1" class="form-control" />
+            <label class="form-label" for="form1Example1">Name</label>
+          </div>
+          <div data-mdb-input-init class="form-outline mb-4">
+            <input v-model="user.email" type="email" id="form1Example1" class="form-control" />
             <label class="form-label" for="form1Example1">Email address</label>
           </div>
 
           <div data-mdb-input-init class="form-outline mb-4">
-            <input type="password" id="form1Example2" class="form-control" />
+            <input v-model="user.password" type="password" id="form1Example2" class="form-control" />
             <label class="form-label" for="form1Example2">Password</label>
           </div>
-
-        
 
           <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block">Sign up</button>
         </form>
       </div>
+      <div class="alert alert-danger" v-if="messagerError" role="alert">
+        {{ messagerError }}
+      </div>
     </div>
   </div>
-  </template>
+</template>
 
-  
 <script>
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { projectAuth, projectFirestore } from '../firebase';
+import { collection, addDoc } from 'firebase/firestore';
+import router from '../router/index';
+
 export default {
   name: 'SignupView',
-  components: {
+  components: {},
+  data() {
+    return {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        uid: ''
+      },
+      ref: collection(projectFirestore, 'users'),
+      messagerError: '',
+      messagerSuccess: ''
+
+    };
+  },
+  methods: {
+    signUp() {
+      createUserWithEmailAndPassword(projectAuth, this.user.email, this.user.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          return addDoc(this.ref, {
+            name: this.user.name,
+            email: this.user.email,
+            uid: user.uid
+          });
+          
+        })
+        
+        .then(() => {
+          this.messagerSuccess='Welcome!',
+          router.push({ name: 'home' });
+          
+        })
+        .catch((error) => {
+          console.error("Error signing up: ", error);
+          this.messagerError = 'Invalid email and password';
+        });
+    }
   }
 }
 </script>
 
 <style>
 </style>
-  
--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -19,24 +19,25 @@
         <li class="nav-item">
           <router-link class="nav-link active" aria-current="page" to="/"  >Home</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item"  v-if="user">
           <router-link class="nav-link active"  aria-current="page" to="/about">About</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item"  v-if="user" >
           <router-link class="nav-link active"  aria-current="page" to="/products">Products</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="user">
           <router-link class="nav-link active"  aria-current="page" to="/my-products">MyProducts</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item"  v-if="!user">
           <router-link class="nav-link active"  aria-current="page" to="/signin">Signin</router-link>
         </li>
-         <li class="nav-item">
+         <li class="nav-item" v-if="!user" >
           <router-link class="nav-link active"  aria-current="page" to="/signup">Signup</router-link>
         </li>
-        <li class="nav-item">
-          <router-link class="nav-link active"  aria-current="page" to="/">Logout</router-link>
+        <li class="nav-item" v-if="user">
+          <a class="nav-link active" href="#" @click.prevent="logout">Logout</a>
         </li>
+       
        
         
         
@@ -53,11 +54,44 @@
 
 <script>
 
+import router from '../router/index';
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { projectAuth } from '../firebase';
 
 export default {
+
   name: 'NavbarView',
   components: {
-  }
+  },
+
+  data() {
+    return {
+      user: null
+    } 
+  },
+  created(){
+    onAuthStateChanged(projectAuth, (userAuth) => {
+    if (userAuth) {
+      console.log('User is logged in:', userAuth);
+      this.user = userAuth;
+    } else {
+      console.log('No user is logged in');
+      this.user = null;
+    }
+  });
+  },
+  methods:{
+    logout(){
+      signOut(projectAuth)
+        .then(() => {
+          router.push({ name: 'signin' });
+        })
+        .catch((error) => {
+          console.error("Error signing out: ", error);
+        });
+    }
+  
+  },
 }
 </script>
 
